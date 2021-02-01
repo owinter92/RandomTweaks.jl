21d2895084fb4e21e668f636d0ce9dec25236614
Documenter.jl <documenter@juliadocs.github.io>
2021-02-01 22:26:37 +0000

    build based on 7230426

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = RandomTweaks","category":"page"},{"location":"#RandomTweaks","page":"Home","title":"RandomTweaks","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [RandomTweaks]","category":"page"}]
+}
