b39ec4fd49381ff1502a0c705879d96ff9c3b38c
Ondrej Winter <ondrej.winter@fs.cvut.cz>
2021-02-05 19:09:23 +0100

    load_file_line_by_line test update.

--- test/load_test.jl
+++ test/load_test.jl
@@ -2,5 +2,5 @@
     file_txt = ["1","2","","a","b c","d e f",""]
 
     @test load_file_line_by_line("data/file.txt") == file_txt
-    #@test_throws ErrorException("File a does not exist.") load_file_line_by_line("a")
+    @test_throws ErrorException("File a does not exist.") load_file_line_by_line("a")
 end
