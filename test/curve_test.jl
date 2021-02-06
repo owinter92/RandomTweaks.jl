8ecf328471c156865ca176823df40b7afe01ff8c
Ondrej Winter <ondrej.winter@fs.cvut.cz>
2021-02-06 10:03:02 +0100

    Updates for curve_length.

--- test/curve_test.jl
+++ test/curve_test.jl
@@ -1,8 +1,7 @@
 @testset "curve_length" begin
-    tol=1_000*eps(Float64)
-
     f(x) = sin(x)
 
     sol = 3.8201977890277120179047620821714432919099676146472747210804966567
-    @test isapprox(curve_length(f,0,pi), sol; atol = tol)
+    @test isapprox(curve_length(f,0,pi), sol; atol = 1e-10)
+    @test !isapprox(curve_length(f,0,5*pi;rtol=1e-4),5*sol,atol=1e-7)
 end
